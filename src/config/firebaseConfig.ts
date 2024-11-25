1f17f1adca9af65a08adb524d52dc3d588ebc6ff
Ionfinisher <teddyassih@gmail.com>
2024-11-25 11:55:55 +0000

    first commit

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,15 +1,15 @@
-// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6Kf35mT6Y3_SHT-g0LQih_Aksj7Bfr-A",
-  authDomain: "languageninja-632a2.firebaseapp.com",
-  projectId: "languageninja-632a2",
-  storageBucket: "languageninja-632a2.firebasestorage.app",
-  messagingSenderId: "88463667195",
-  appId: "1:88463667195:web:6019aba62132d15a37e55e",
-  measurementId: "G-0TKP3CJ4K6",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
